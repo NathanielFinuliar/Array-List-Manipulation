d92c124448468310b79efcf589827287265dfe86
NathanielFinuliar <nathaniel.finuliar14@gmail.com>
2023-03-01 01:14:20 +0800

    Added Header for the program

---
+++ arraylistmethods.py
@@ -0,0 +1,10 @@
+import random as rd
+from collections import deque
+
+print("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-")
+print("|"," "*16, " "*28, "|")
+print("|"," "* 10, "ARRAY LIST MANIPULATION", " " *10,"|")
+print("|"," " * 8, "coded by Nathaniel Finuliar", " " *8,"|")
+print("|"," " * 16, "BSCOE 2-2", " " * 18, "|")
+print("|"," "*16, " "*28, "|")
+print("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-")
