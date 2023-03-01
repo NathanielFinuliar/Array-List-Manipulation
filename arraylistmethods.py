138f8aa983ca92bf48a13d9565cd18177d26a849
NathanielFinuliar <nathaniel.finuliar14@gmail.com>
2023-03-01 19:44:38 +0800

    Added comments for some functions

--- arraylistmethods.py
+++ arraylistmethods.py
@@ -1,4 +1,4 @@
-import random as rd
+import random as rd 
 from collections import deque
 
 print("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-")
@@ -46,8 +46,12 @@ def add_element(main_array):
 
 
 def insert_element(main_array):
+    # If the index is exceed length of the array it will just throw the item to the end of the array
+    # This method recieves input from user with is seperated by space ex. "1 2 3 4 5 6"
     while True:
         try:
+            # Create two variables to hold that seperated value, in this case
+            # We need two variables because we need two value (index,value)
             index,value = [int(i) for i in input("Enter the index follow up by the value you want to add: ").split()]
             main_array.insert(index,value)
             print(f"{value} has been added to array at the index of {index}")
@@ -70,12 +74,13 @@ def remove_element(main_array):
     while True:
         try:
             option_del = int(input("Do you want to delete by value or index (value : 1 , index : 2) : "))
+            # Here we seperated to two cases
             if option_del == 1:
-                item = int(input("What do you want to delete ? : "))
-                array_.remove(item)
+                item = int(input("What do you want to delete ? : ")) 
+                array_.remove(item)  # remove will delete the element by value
             elif option_del == 2:
                 index_del = int(input("Which index do you want to delete ? : "))
-                del main_array[index_del]
+                del main_array[index_del] # remove will delete the element by index
             return
         except:
             print("Invalid input try again!")
@@ -90,7 +95,7 @@ def reverse_array(main_array):
 
 def rotate_element(main_array):
     global array_ 
-    array_= deque(array_)
+    array_= deque(array_)  # Convert array to deque object make it usable for deque's methods
     valid_step= False 
     valid_side= False
     while not valid_step:
@@ -109,12 +114,13 @@ def rotate_element(main_array):
         side = input("Which way you want to rotate? (L for Left / R for Right) : ")
         if side.upper() == 'L' or side.upper() == 'R':
             if side.upper() == 'L':
-                array_.rotate(-abs(step))
+                array_.rotate(-abs(step))  # Set the -abs here for left rotate becuase left rotate need negative integer
         elif side.upper() == 'R':
-            array_.rotate(abs(step))
-        valid_side = True
+            array_.rotate(abs(step)) # Same as above but right need positive integer
+        valid_side = True #If this scope run we set this boolean to True so, the loop won't run again
     else:
         print("Invalid input try again!")
+    # Convert array_ to list due of converting array_ to deque object
     array_ = list(array_)
 
 def shuffle_array(main_array):
@@ -129,13 +135,15 @@ def clear_array(main_array):
     else:
         print("Array is already emptied")
 
-
+# Created dictionary that carries function in it
+# I use integer number for a key and functions's names as value
 menu_function = {1:add_element,2:insert_element,3:modify_element,
                  4:remove_element,5:sort_array,6:reverse_array, 
                  7:rotate_element, 8:shuffle_array,9:clear_array}
 
+# In order to access function from dictionary
 def function_runner():
-    menu_function[_start(array_,menu_option)](array_)
+    menu_function[_start(array_,menu_option)](array_) 
     print(f"This is the new array : {array_}")
 
 exit_ = False
